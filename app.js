40b5b6c5c984f802955069d56bb725f5e872b563
ryusei <117654409+ryusei00734@users.noreply.github.com>
2022-11-23 22:27:52 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+for (let i = 0;  i<=50;  i++) {
+    console.log(i);
+
+    if(i % 10 === 0) {
+        console.log(`今${i}回ループしました。`)
+    }else if(i % 4 === 0) {
+        console.log(`4で割れる数字です。` + [i])
+    }else (i % 50 === 0); {
+        console.log(`50回カウントが終わりました。`)
+    }
+}
+
+alert(`50回カウントが終わりました。`);
